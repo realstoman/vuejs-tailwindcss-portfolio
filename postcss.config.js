0ddf35c29076a3e1b861c6170254dbf2f99295a7
nstoman <nangialaistoman7@gmail.com>
2021-05-20 13:52:18 +0430

    Post css issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    // require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
