8b250a1191733d745e5501960e2235a8e99abf5f
nstoman <nangialaistoman7@gmail.com>
2021-05-20 13:55:03 +0430

    Postcss issue resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
