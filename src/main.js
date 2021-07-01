a30198a1ed38b4df2e566234dbe54028b3feb4b7
nstoman <nangialaistoman7@gmail.com>
2021-07-01 16:51:43 +0430

    Add feather icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
 
+const feather = require('feather-icons')
+feather.replace()
+
 createApp(App).mount('#app')
