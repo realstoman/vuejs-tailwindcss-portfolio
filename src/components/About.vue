73991b41623e9fe01bd950c5d2f209533b2285ed
nstoman <nangialaistoman7@gmail.com>
2021-07-01 16:53:44 +0430

    Create about component

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <i data-feather="home"></i>
+    </div>
+</template>
+
+<script>
+import feather from 'feather-icons'
+
+export default {
+    name: 'Home',
+    props: {
+        msg: String
+    },
+    mounted() {
+        feather.replace();
+    },
+    updated() {
+        feather.replace();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
