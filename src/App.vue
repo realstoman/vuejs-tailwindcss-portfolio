9faee643d911001d3ad907a55f79a91bb66a0b80
nstoman <nangialaistoman7@gmail.com>
2021-07-01 17:04:01 +0430

    Add vue router and routes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <Home msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
 import App from './App.vue'
 import './assets/css/tailwind.css'
+import Home from './components/Home'
+import About from './components/About'
+import router from './router'
 
 const feather = require('feather-icons')
 feather.replace()
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App).use(router).use(router).mount('#app')
