c1eaac823a5f60aa31054af2a7a6e1f29bb1a249
nstoman <nangialaistoman7@gmail.com>
2021-07-03 15:11:26 +0430

    Add illustration and home layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="container mx-auto px-4">
-    <p>Home Page</p>
-    <i data-feather="home"></i>
+  <div class="container mx-auto px-4 py-10">
+    <!-- Banner Start -->
+    <div class="flex flex-col sm:flex-row justify-between">
+      <div class="w-full sm:w-1/2 text-left">
+        <h1>Hi Iam Stoman</h1>
+      </div>
+      <div class="w-full sm:w-1/2 text-right">
+        <img src="@/assets/images/developer.svg" alt="Developer">
+      </div>
+    </div>
+    <!-- Banner End -->
+
+    <!-- Banner Start -->
+
   </div>
 </template>
 
