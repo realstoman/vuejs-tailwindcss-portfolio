e4cd080bab22cecb37072cbb945e02a74c9070c8
nstoman <nangialaistoman7@gmail.com>
2021-07-16 00:08:57 +0430

    REmove unneeded pages

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () =>
-			import(/* webpackChunkName: "about" */ '../components/About.vue'),
+			import(/* webpackChunkName: "about" */ '../views/About.vue'),
 		meta: {
 			title: 'Stoman - About',
 		},
