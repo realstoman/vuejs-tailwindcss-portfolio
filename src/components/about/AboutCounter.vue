e989a57870b4e2f8829693059b75ac0813d237f9
nstoman <nangialaistoman7@gmail.com>
2021-07-16 20:14:01 +0430

    About responsive design

--- src/components/about/AboutCounter.vue
+++ src/components/about/AboutCounter.vue
@@ -1,15 +1,17 @@
 <template>
-	<div class="mt-20 bg-gray-100 shadow-sm">
-		<div class="container mx-auto p-10 flex justify-between items-center">
-			<div>
+	<div class="mt-10 sm:mt-20 bg-gray-100 shadow-sm">
+		<div
+			class="container mx-auto p-10 block sm:flex sm:justify-between sm:items-center"
+		>
+			<div class="mb-20 sm:mb-0">
 				<p class="text-4xl font-bold text-gray-600 mb-2">12</p>
 				<span class="text-md text-gray-500">Years of experience</span>
 			</div>
-			<div>
+			<div class="mb-20 sm:mb-0">
 				<p class="text-4xl font-bold text-gray-600 mb-2">20k+</p>
 				<span class="text-md text-gray-500">Stars on GitHub</span>
 			</div>
-			<div>
+			<div class="mb-20 sm:mb-0">
 				<p class="text-4xl font-bold text-gray-600 mb-2">92%</p>
 				<span class="text-md text-gray-500">Positive feedback</span>
 			</div>
