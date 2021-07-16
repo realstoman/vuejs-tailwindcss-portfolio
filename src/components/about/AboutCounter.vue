70379157b50c6b83cdceaa40a79aed45175c65c0
nstoman <nangialaistoman7@gmail.com>
2021-07-16 19:58:06 +0430

    About components and structure

---
+++ src/components/about/AboutCounter.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="flex justify-between items-center">
+		<div>
+			<p>12</p>
+			<span>Years of experience</span>
+		</div>
+		<div>
+			<p>20k+</p>
+			<span>Stars on GitHub</span>
+		</div>
+		<div>
+			<p>92%</p>
+			<span>Positive feedback</span>
+		</div>
+		<div>
+			<p>77</p>
+			<span>Projects completed</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {},
+};
+</script>
---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div class="w-full sm:w-1/3">
+			<img src="" alt="" />
+		</div>
+		<div class="w-full sm:w-2/3">
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+				vel illum asperiores dignissimos cumque quibusdam et fugiat
+				voluptatem nobis suscipit explicabo, eaque consequatur nesciunt,
+				fugit eligendi corporis laudantium adipisci soluta?
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {},
+};
+</script>
