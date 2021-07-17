66ccfe6cb1c1200702e76a879a50a337dbe896f1
nstoman <nangialaistoman7@gmail.com>
2021-07-17 20:29:41 +0430

    Tailwind config changes for colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				background: {
-					'primary-light': '#F7F8FC',
-					'secondary-light': '#FFFFFF',
-					'ternary-light': '#f6f7f8',
-					'primary-dark': '#0D2438',
-					'secondary-dark': '#102D44',
-					'ternary-dark': '#1E3851',
-				},
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
 			},
 		},
 		container: {
