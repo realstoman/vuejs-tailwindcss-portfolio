613e483690e5cb47e0978ec2ec4c5481446955b6
nstoman <nangialaistoman7@gmail.com>
2021-07-19 22:38:29 +0430

    Add new banner illustration

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- Banner start -->
 	<section
-		class="flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-20"
+		class="flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-2"
 	>
 		<div class="w-full sm:w-1/3 text-left">
 			<h1
@@ -28,7 +28,7 @@
 			</div>
 		</div>
 		<div class="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0">
-			<img src="@/assets/images/dev.svg" alt="Developer" />
+			<img src="@/assets/images/developer.svg" alt="Developer" />
 		</div>
 	</section>
 	<!-- Banner end -->
