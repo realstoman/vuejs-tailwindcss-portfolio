2a1bed00c43d257c2620c06c6a9114d513f1d1d3
NangialaiStoman <nstoman7@gmail.com>
2021-10-30 00:25:00 +0430

    Composables changes

--- src/composables/useThemeSwitcher.js
+++ src/composables/useThemeSwitcher.js
@@ -15,67 +15,12 @@ export default function useThemeSwitcher() {
 	function setLightTheme() {
 		currentTheme.value = 'light';
 
-		document.documentElement.style.setProperty(
-			'--primary',
-			'var(--purple)'
-		);
-		document.documentElement.style.setProperty(
-			'--background',
-			'var(--bg--light)'
-		);
-		document.documentElement.style.setProperty(
-			'--text',
-			'var(--text--light'
-		);
-		document.documentElement.style.setProperty(
-			'--link-text',
-			'var(--link-text--light'
-		);
-		document.documentElement.style.setProperty(
-			'--active-link-text',
-			'var(--active-link-text--light'
-		);
-		document.documentElement.style.setProperty(
-			'--shadow',
-			'var(--shadow--light'
-		);
-		document.documentElement.style.setProperty(
-			'--quote-bg',
-			'var(--quote-bg--light'
-		);
-
 		process.isClient && localStorage.setItem('theme', 'light');
 	}
 
 	// Dark Theme Function
 	function setDarkTheme() {
 		currentTheme.value = 'dark';
-
-		document.documentElement.style.setProperty('--primary', 'var(--teal)');
-		document.documentElement.style.setProperty(
-			'--background',
-			'var(--bg--dark)'
-		);
-		document.documentElement.style.setProperty(
-			'--text',
-			'var(--text--dark'
-		);
-		document.documentElement.style.setProperty(
-			'--link-text',
-			'var(--link-text--dark'
-		);
-		document.documentElement.style.setProperty(
-			'--active-link-text',
-			'var(--active-link-text--dark'
-		);
-		document.documentElement.style.setProperty(
-			'--shadow',
-			'var(--shadow--dark'
-		);
-		document.documentElement.style.setProperty(
-			'--quote-bg',
-			'var(--quote-bg--dark'
-		);
 
 		process.isClient && localStorage.setItem('theme', 'dark');
 	}
