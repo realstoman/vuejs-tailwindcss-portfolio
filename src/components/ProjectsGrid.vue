d43cddcec8c6cea0cec9ffebbe3015e9216cebf7
NangialaiStoman <nstoman7@gmail.com>
2021-12-01 23:09:52 +0430

    Projects grid responsive

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -87,7 +87,9 @@
 		<!-- Filter and search projects end -->
 
 		<!-- Projects grid start -->
-		<div class="grid grid-cols-1 sm:grid-cols-3 mt-6 sm:gap-10">
+		<div
+			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10"
+		>
 			<ProjectSingle
 				v-for="project in filteredProjects"
 				:key="project.id"
