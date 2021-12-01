194f0452c004448a58a14723cf904822f7612eec
NangialaiStoman <nstoman7@gmail.com>
2021-12-01 23:51:52 +0430

    Fix conflict

--- src/components/AboutClientSingle.vue
+++ src/components/AboutClientSingle.vue
@@ -3,7 +3,7 @@
 		<img
 			:src="client.img"
 			:alt="client.title"
-			class="w-64 py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-xl mb-8 cursor-pointer bg-secondary-light"
+			class="w-64 py-5 px-10 border border-ternary-light dark:border-ternary-dark shadow-sm rounded-xl mb-8 cursor-pointer dark:bg-secondary-light"
 		/>
 	</div>
 </template>
