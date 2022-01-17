26e8c128591b05592360569bbad4bf643e3ca641
realstoman <realstoman@gmail.com>
2022-01-17 12:40:02 +0430

    Header and banner fonts appplied

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -6,12 +6,12 @@
 		<!-- Banner left contents -->
 		<div class="w-full md:w-1/3 text-left">
 			<h1
-				class="text-3xl md:text-4xl xl:text-5xl text-center sm:text-left font-semibold text-ternary-dark dark:text-primary-light uppercase"
+				class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left font-semibold text-ternary-dark dark:text-primary-light uppercase"
 			>
 				Hi, Iam Stoman
 			</h1>
 			<p
-				class="mt-4 text-xl md:text-2xl xl:text-4xl text-center sm:text-left font-semibold leading-none text-gray-400"
+				class="font-general-regular mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left font-semibold leading-none text-gray-400"
 			>
 				A Full-Stack Developer & Design Enthusiast
 			</p>
@@ -26,7 +26,9 @@
 						data-feather="arrow-down-circle"
 						class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6"
 					></i>
-					<span class="text-sm sm:text-lg">Download CV</span></a
+					<span class="text-sm sm:text-lg font-general-medium"
+						>Download CV</span
+					></a
 				>
 			</div>
 		</div>
--- src/components/shared/AppHeaderLinks.vue
+++ src/components/shared/AppHeaderLinks.vue
@@ -6,19 +6,19 @@
 	>
 		<router-link
 			to="/projects"
-			class="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
+			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 "
 			aria-label="Projects"
 			>Projects</router-link
 		>
 		<router-link
 			to="/about"
-			class="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 			aria-label="About Me"
 			>About Me</router-link
 		>
 		<router-link
 			to="/contact"
-			class="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 			aria-label="Contact"
 			>Contact</router-link
 		>
@@ -26,7 +26,7 @@
 			class="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 		>
 			<button
-				class="sm:hidden block text-left text-md font-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2"
+				class="font-general-medium sm:hidden block text-left text-md font-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2"
 				@click="showModal()"
 				aria-label="Hire Me Button"
 			>
