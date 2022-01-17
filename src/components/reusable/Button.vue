6b4068099cd322d7362aa8e4d9d8e8b7e518ec3d
realstoman <realstoman@gmail.com>
2022-01-17 13:54:13 +0430

    Button reuable component

---
+++ src/components/reusable/Button.vue
@@ -0,0 +1,16 @@
+<template>
+	<button>{{ title }}</button>
+</template>
+
+<script>
+export default {
+	props: ['title'],
+	data: () => {
+		return {
+			//
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,9 @@
 				to="/projects"
 				class="font-general-medium flex items-center px-6 py-3 rounded-xl shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl"
 				aria-label="More Projects"
-				>More Projects</router-link
 			>
+				<Button title="More Projects" />
+			</router-link>
 		</div>
 	</div>
 </template>
@@ -21,12 +22,14 @@
 <script>
 import AppBanner from '@/components/shared/AppBanner';
 import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
+import Button from '../components/reusable/Button.vue';
 
 export default {
 	name: 'Home',
 	components: {
 		AppBanner,
 		ProjectsGrid,
+		Button,
 	},
 };
 </script>
