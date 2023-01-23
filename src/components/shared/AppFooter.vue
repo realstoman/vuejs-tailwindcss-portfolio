194096e8019a1fe7ba9337bf348d59af35cc9681
realstoman <realstoman@gmail.com>
2023-01-23 11:53:15 +0430

    Update footer links and move links data to file

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,43 +1,13 @@
 <script>
 import feather from 'feather-icons';
 import FooterCopyright from './FooterCopyright.vue';
+import { socialLinks } from '../../data/socialLinks';
 
 export default {
 	components: { FooterCopyright },
 	data() {
 		return {
-			socials: [
-				{
-					id: 1,
-					name: 'Website',
-					icon: 'globe',
-					url: 'https://www.stoman.me/',
-				},
-				{
-					id: 2,
-					name: 'GitHub',
-					icon: 'github',
-					url: 'https://github.com/realstoman',
-				},
-				{
-					id: 3,
-					name: 'Twitter',
-					icon: 'twitter',
-					url: 'https://twitter.com/realstoman',
-				},
-				{
-					id: 4,
-					name: 'LinkedIn',
-					icon: 'linkedin',
-					url: 'https://www.linkedin.com/in/realstoman',
-				},
-				{
-					id: 5,
-					name: 'YouTube',
-					icon: 'youtube',
-					url: 'https://www.youtube.com/c/realstoman',
-				},
-			],
+			socials: socialLinks,
 		};
 	},
 	mounted() {
